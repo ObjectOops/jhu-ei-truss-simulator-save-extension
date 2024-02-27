b9e9fa469c1267b74cb306015e5926ea627a2c5a
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-02-27 18:00:00 +0000

    Add script to generate a local client if bookmarklets cannot be used for some reason. This is a polyglot script!

---
+++ client_generator/generator.bash
@@ -0,0 +1,22 @@
+
+# Polyglot madness.
+
+wget -r -np "https://ei.jhu.edu/truss-simulator/"
+
+wget -P "ei.jhu.edu/cdn-cgi/scripts/5c5dd728/cloudflare-static" "https://ei.jhu.edu/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
+wget -P "ei.jhu.edu/cdn-cgi/scripts/7d0fa10a/cloudflare-static" "https://ei.jhu.edu/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+
+# Note: Take care to only use double quotes inside the embedded script, or escape single quotes with '\''.
+
+python <(echo '
+
+fin = open("ei.jhu.edu/truss-simulator/index.html", "r")
+raw_content = fin.read()
+modified_content = raw_content.replace("/cdn-cgi", "../cdn-cgi").replace("</body>", "<script>window.__cfRLUnblockHandlers = true;</script><script src=\"../../../bookmarklet/save_extension.js\"></script></body>")
+fin.close()
+
+fout = open("ei.jhu.edu/truss-simulator/index.html", "w")
+fout.write(modified_content)
+fout.close()
+
+')
