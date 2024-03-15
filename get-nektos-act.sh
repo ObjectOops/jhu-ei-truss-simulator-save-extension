f4935f6d919efa7e991499153a541a2c354a0acb
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-03-15 19:14:21 +0000

    Fix shebang and .gitignore.

--- get-nektos-act.sh
+++ get-nektos-act.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+
+ACT_VERSION="v0.2.60"
 
 mkdir nektos_act
 cd nektos_act
-wget "https://github.com/nektos/act/releases/download/v0.2.60/act_Linux_x86_64.tar.gz"
+wget "https://github.com/nektos/act/releases/download/${ACT_VERSION}/act_Linux_x86_64.tar.gz"
 tar -xzf act_Linux_x86_64.tar.gz
