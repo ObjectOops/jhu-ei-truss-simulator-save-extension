e46062f81d29bcbee58ae5962281534f273e7422
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-03-18 18:00:00 +0000

    Replace unnecessary whitespace to shorten bookmarklet URL.

--- bookmarklet/condense.js
+++ bookmarklet/condense.js
@@ -3,6 +3,8 @@ const fs = require('fs').promises;
 (async function() {
     try {
         let content = await fs.readFile('./save_extension.js', 'utf8');
+        content = content.replace(new RegExp("\\n|\\s\\s\\s\\s", "g"), "");
+        // console.log(content);
         console.log("javascript:" + encodeURIComponent(content));
     } catch (error) {
         console.log(error);
