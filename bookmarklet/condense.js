c5681a8c1529e181ba6b9667d0c69f7d71d5d2f3
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-03-19 19:10:50 +0000

    Fix condenser. May invest in using a minifier later.

--- bookmarklet/condense.js
+++ bookmarklet/condense.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 (async function() {
     try {
         let content = await fs.readFile('./save_extension.js', 'utf8');
-        content = content.replace(new RegExp("\\n|\\s\\s\\s\\s", "g"), "");
+        content = content.replace(new RegExp("\\ {4}", "g"), "");
         // console.log(content);
         console.log("javascript:" + encodeURIComponent(content));
     } catch (error) {
