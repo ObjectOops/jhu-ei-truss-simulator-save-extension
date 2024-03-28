c4947e0958930991b4ed79f5f7c49047f460b325
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-03-28 18:00:00 +0000

    Use an actual minifier.

--- bookmarklet/condense.js
+++ bookmarklet/condense.js
@@ -1,11 +1,13 @@
 const fs = require('fs').promises;
+const minify = require('@minify-js/node');
 
 (async function() {
     try {
         let content = await fs.readFile('./save_extension.js', 'utf8');
-        content = content.replace(new RegExp("\\ {4}", "g"), "");
-        // console.log(content);
-        console.log("javascript:" + encodeURIComponent(content));
+        // content = content.replace(new RegExp("\\ {4}", "g"), "");
+        buffer = minify.minify("global", Buffer.from(content));
+        // console.log(buffer.toString());
+        console.log("javascript:" + encodeURIComponent(buffer.toString()));
     } catch (error) {
         console.log(error);
     }
